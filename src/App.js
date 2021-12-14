1f87befdd64850cd91169e7dc6c9494237296bb1
plarrat <larrat.philippe@lessonsharing.fr>
2021-12-14 16:43:05 +0100

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import NavbarApp from "./components/NavbarApp"
+import { Container, Row, Col, InputGroup, FormControl, Button, Image } from "react-bootstrap"
 
-function App() {
+export default function App() {
+
+  const [nbPic, setNbPic] = useState(1)
+  const [pics, setPics] = useState([])
+
+  function getDatas(){
+    let url = `https://picsum.photos/v2/list?limit=${nbPic}`
+
+    fetch(url).then(function(res){
+      return res.json()
+    }).then(function(datas){
+      setPics(datas)
+    })
+    
+  }
+
+  function displayPics(){
+    return pics.map(pic=>{
+    
+      return (
+        <Col xs={6} md={4} className="m-3">
+            <Image src={pic.download_url} width="200 " rounded />
+        </Col>
+      )
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarApp />
+
+      <Container >
+        <Row className="mt-5">
+          <Col md="6">
+            <h1>Exemple d'appel API </h1>
+            <hr />
+
+             <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Saisir un nombre de photos"
+                value={nbPic}
+                onChange={(e)=>{setNbPic(e.target.value)}}
+              />
+                <InputGroup.Text>Nombre de Photos ({pics.length})</InputGroup.Text>
+              </InputGroup>
+
+              <Button variant="info" onClick={getDatas}>Cliquez moi</Button>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          {displayPics()}
+        </Row>
+      </Container>
     </div>
   );
 }
 
-export default App;
+
