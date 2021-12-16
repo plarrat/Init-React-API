8376e4641154dbb11e2aca0d06241f2ee3463b3e
plarrat <larrat.philippe@lessonsharing.fr>
2021-12-16 15:32:07 +0100

    Fromulaire ajout

--- src/App.js
+++ src/App.js
@@ -1,62 +1,87 @@
-import './App.css';
-import {useState} from "react"
+import "./App.css"
+import { useState } from "react"
 import NavbarApp from "./components/NavbarApp"
-import { Container, Row, Col, InputGroup, FormControl, Button, Image } from "react-bootstrap"
+import {
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  FormControl,
+  Button,
+  Form,
+} from "react-bootstrap"
 
 export default function App() {
-
   const [nbPic, setNbPic] = useState(1)
   const [produit, setProduit] = useState({})
 
-  function getDatas(){
-    let url = `https://world.openfoodfacts.org/api/v0/product/3229820791074.json`
+  function getDatas() {
+    let url = `http://localhost:3010/user/${nbPic}`
 
-    fetch(url).then(function(res){
-      return res.json()
-    }).then(function(datas){
-      setProduit(datas)
-    })
-    
+    fetch(url)
+      .then(function (res) {
+        return res.json()
+      })
+      .then(function (datas) {
+        alert()
+        // setProduit(datas)
+      })
   }
-
 
   return (
     <div className="App">
       <NavbarApp />
 
-      <Container >
+      <Container>
         <Row className="mt-5">
           <Col md="6">
             <h1>Exemple d'appel API </h1>
             <hr />
 
-             <InputGroup className="mb-3">
+            <InputGroup className="mb-3">
               <FormControl
                 placeholder="Saisir un nombre de photos"
                 value={nbPic}
-                onChange={e=>setNbPic(e.target.value)}
+                onChange={(e) => setNbPic(e.target.value)}
               />
-                <InputGroup.Text>Nombre de Photos {produit.length}</InputGroup.Text>
-              </InputGroup>
+              <InputGroup.Text>
+                Nombre de Photos {produit.length}
+              </InputGroup.Text>
+            </InputGroup>
 
-              <Button variant="info" onClick={getDatas}>Cliquez moi</Button>
+            <Button variant="info" onClick={getDatas}>
+              Cliquez moi
+            </Button>
           </Col>
         </Row>
       </Container>
 
       <Container>
-        <Row>
-          {(produit.product != null) &&
-            <Col xs={6} md={4} className="m-3">
-              <h3>{produit.product.brands}</h3>
-              <h3>{produit.product.prodcut_name_fr}</h3>
-              <Image src={produit.product.image_small_url} width="200 " rounded />
-            </Col>
-          }
+        <Row mt={5}>
+          <Col md="6">
+            <h1>Formulaire ajout utilisateur</h1>
+            <hr />
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>Nom</Form.Label>
+                <Form.Control type="nom" placeholder="Larrat" />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Prenom</Form.Label>
+                <Form.Control type="prenom" placeholder="Philippe" />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Age</Form.Label>
+                <Form.Control type="age" placeholder="ex : 20" />
+              </Form.Group>
+
+              <Button variant="info">Enregistrer</Button>
+            </Form>
+          </Col>
         </Row>
       </Container>
     </div>
-  );
+  )
 }
-
-
